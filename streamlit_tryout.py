d9c0f0b9fdde5299095a270313fd4f6281776052
wgurira <134268281+wgurira@users.noreply.github.com>
2023-09-29 19:20:15 +0200

    Update streamlit_tryout.py

--- streamlit_tryout.py
+++ streamlit_tryout.py
@@ -1,15 +1,14 @@
 import streamlit as st
-from tensorflow.keras.preprocessing.sequence import pad_sequences
-from keras.models import load_model
-import pickle
-import numpy as np
 import cv2
+import numpy as np
+from tensorflow.keras.models import load_model
+import pickle
 
 # Load the image captioning model and tokenizer
-caption_model = load_model("myicmodel.h5")
-tokenizer = pickle.load(open("myictokenizer.pkl", "rb"))
+caption_model = load_model('myicmodel.h5')
+tokenizer = pickle.load(open('myictokenizer.pkl', 'rb'))
 
-# Function to preprocess frames and generate captions
+# Define function to preprocess frames and generate captions
 def preprocess_frame(frame):
     # Preprocess the frame (resize, normalize, etc.)
     # ...
@@ -20,18 +19,8 @@ def preprocess_frame(frame):
     # Return the generated caption
     # ...
 
-# Function to generate next word predictions
-def generate_predictions(seed_text, model, tokenizer, max_sequence_len=8, next_words=5):
-    for _ in range(next_words):
-        token_list = tokenizer.texts_to_sequences([seed_text])[0]
-        token_list = pad_sequences([token_list], maxlen=max_sequence_len-1, padding='pre')
-        predicted_probs = model.predict(token_list)
-        predicted_word = tokenizer.index_word[np.argmax(predicted_probs)]
-        seed_text += " " + predicted_word
-    return seed_text
-
 # Streamlit UI
-st.title("Video Captioning and Word Prediction App")
+st.title("Video Captioning App")
 
 # Allow user to upload a video file
 video_file = st.file_uploader("Upload a video (max 2MB)", type=["mp4"])
@@ -52,12 +41,8 @@ if video_file:
             # Preprocess the frame and generate caption
             caption = preprocess_frame(frame)
 
-            # Generate word predictions based on caption
-            predictions = generate_predictions(caption, caption_model, tokenizer)
-
-            # Display the frame and its caption with word predictions
-            st.image(frame, caption=f"Caption: {caption}")
-            st.write("Word Predictions:", predictions)
+            # Display the frame and its caption
+            st.image(frame, caption=caption)
 
     except Exception as e:
         st.error(f"An error occurred: {e}")
