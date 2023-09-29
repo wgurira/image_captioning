6cf2dd5ccc50b7d08d9e452d15ccd0db506122c5
wgurira <134268281+wgurira@users.noreply.github.com>
2023-09-29 19:16:27 +0200

    Update streamlit_tryout.py

--- streamlit_tryout.py
+++ streamlit_tryout.py
@@ -1,14 +1,15 @@
 import streamlit as st
-import cv2
-import numpy as np
-from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from keras.models import load_model
 import pickle
+import numpy as np
+import cv2
 
 # Load the image captioning model and tokenizer
-caption_model = load_model('model.h5')
-tokenizer = pickle.load(open('tokenizer.pkl', 'rb'))
+caption_model = load_model("myicmodel.h5")
+tokenizer = pickle.load(open("myictokenizer.pkl", "rb"))
 
-# Define function to preprocess frames and generate captions
+# Function to preprocess frames and generate captions
 def preprocess_frame(frame):
     # Preprocess the frame (resize, normalize, etc.)
     # ...
@@ -19,37 +20,46 @@ def preprocess_frame(frame):
     # Return the generated caption
     # ...
 
-# Define the Streamlit app
-def main():
-    # Set Streamlit app title
-    st.title("Video Captioning App")
+# Function to generate next word predictions
+def generate_predictions(seed_text, model, tokenizer, max_sequence_len=8, next_words=5):
+    for _ in range(next_words):
+        token_list = tokenizer.texts_to_sequences([seed_text])[0]
+        token_list = pad_sequences([token_list], maxlen=max_sequence_len-1, padding='pre')
+        predicted_probs = model.predict(token_list)
+        predicted_word = tokenizer.index_word[np.argmax(predicted_probs)]
+        seed_text += " " + predicted_word
+    return seed_text
 
-    # Allow user to upload a video file
-    video_file = st.file_uploader("Upload a video (max 2MB)", type=["mp4"])
+# Streamlit UI
+st.title("Video Captioning and Word Prediction App")
 
-    if video_file:
-        try:
-            # Read the video file using OpenCV
-            video = cv2.VideoCapture(video_file)
+# Allow user to upload a video file
+video_file = st.file_uploader("Upload a video (max 2MB)", type=["mp4"])
 
-            # Iterate over each frame
-            while True:
-                # Read the next frame
-                ret, frame = video.read()
+if video_file:
+    try:
+        # Read the video file using OpenCV
+        video = cv2.VideoCapture(video_file)
 
-                if not ret:
-                    break
+        # Iterate over each frame
+        while True:
+            # Read the next frame
+            ret, frame = video.read()
 
-                # Preprocess the frame and generate caption
-                caption = preprocess_frame(frame)
+            if not ret:
+                break
 
-                # Display the frame and its caption
-                st.image(frame, caption=caption)
-        except Exception as e:
-            st.error(f"An error occurred: {e}")
-    else:
-        st.warning("Please upload a video file.")
+            # Preprocess the frame and generate caption
+            caption = preprocess_frame(frame)
 
-# Run the Streamlit app
-if __name__ == '__main__':
-    main()
+            # Generate word predictions based on caption
+            predictions = generate_predictions(caption, caption_model, tokenizer)
+
+            # Display the frame and its caption with word predictions
+            st.image(frame, caption=f"Caption: {caption}")
+            st.write("Word Predictions:", predictions)
+
+    except Exception as e:
+        st.error(f"An error occurred: {e}")
+else:
+    st.warning("Please upload a video file.")
